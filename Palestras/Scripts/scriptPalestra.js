1939243d9d314e7e9c432a3581535013bace104d
anbeatrizduarte <bana2229@gmail.com>
2025-09-29 03:16:24 -0300

    Palestra: adicionadas as informações reais

---
+++ Palestras/Scripts/scriptPalestra.js
@@ -0,0 +1,91 @@
+
+
+const diasPalestra = {
+    1: [
+        { img: "img/palestras/andre.png", palestrante: "André Bessa", titulo: "Computação: Reflexões Sobre o Passado e Pistas Sobre o Futuro", local: "Auditório", horario: "", sobre: "André Bessa é Desenvolvedor de Software Sênior com mestrado em Ciência da Computação. Possui mais de 9 anos de experiência em Java, mais de 3 anos em Python e mais de 5 anos em C/C++. Utiliza metodologias ágeis desde 2014. É apaixonado por desenvolver aplicações que geram valor para os clientes e têm um impacto positivo na vida das pessoas. Atualmente, trabalha na DATAPREV como Desenvolvedor de Software Sênior, atuando em diferentes fases do ciclo de vida do desenvolvimento de software (SDLC). Suas atividades estão principalmente relacionadas ao desenvolvimento de funcionalidades tanto no front-end quanto no back-end." },
+        { img: "img/palestras/edmilson.png", palestrante: "Edmilson Queiroz", titulo: "IoT & IA: Como essas tecnologias estão transformando a indústria e sociedade?", local: "Auditório", horario: "", sobre: "Edmilson Queiroz é Doutorando em Engenharia Elétrica (Capes 5) pela Universidade Federal do Espírito Santo. Mestre em Engenharia de Teleinformática (Capes 6) pela Universidade Federal do Ceará (2013). Possui graduação em Tecnologia Mecatrônica Industrial pelo Centro Federal de Educação Tecnológica do Ceará (2007.2). Tendo desenvolvido projetos de automação na indústria local cearense e atuou como docente em diversas instituições de Fortaleza, como por exemplo, SENAI-CE, IFCE, UNI7, FBUNI, UNIFAMETRO e Faculdade Ari de Sá." }
+    ],
+
+    2: [
+        { img: "img/palestras/eliseu.png", palestrante: "Eliseu Castelo", titulo: "Privacidade de Dados na Nuvem", local: "Auditório", horario: "", sobre: "Eliseu Castelo possui graduação em Bacharelado em Filosofia pela Universidade Estadual do Ceará (1992) e Psicologia pelo Centro Universitário Estácio do Ceará (2024) e mestrado em Informática Aplicada pela Universidade de Fortaleza (2001). Doutorado em Ciências da Computação pela Universidade Federal do Ceará-UFC (2017).  Tem experiência na área de Ciência da Computação, com ênfase em Sistemas de Informação, atuando principalmente nos seguintes temas: planejamento estratégico, gerência de projetos, análise multicritério, cloud computing, privacidade de dados e avaliação de qualidade e processos de software." },
+        { img: "img/palestras/wendell.png", palestrante: "Wendell Rodrigues", titulo: "Cibersegurança em Foco: Principais Ameaças, Conformidade com NIST CSF 2.0 e o Papel do SIEM/SOAR na Defesa Ativa", local: "Auditório", horario: "", sobre: "O prof. Wendell Rodrigues é doutor em Computação pela Universidade de Lille 1, França, com pós-doutorado em Gestão da Inovação pelo Lambton College, Canadá. Atua como professor titular no Instituto Federal de Tecnologia do Ceará, lecionando nos cursos de Engenharia e Mestrado da Computação e de Telecomunicações. Com ampla experiência em engenharia de software, computação de alto desempenho e segurança de redes, é pesquisador no LIT e consultor de Inteligência Artificial no NEPEN." },
+        { img: "img/palestras/matheus.png", palestrante: "Matheus Paixão", titulo: "Mestrado e Doutorado no PPGCC: Oportunidades para Além da Carreira Acadêmica", local: "Auditório", horario: "", sobre: "Matheus Esteves Paixão é Professor Adjunto da Universidade Estadual do Ceará, Engenheiro de Software e Cientista da Computação. Também é membro permanente do Programa de Pós-Graduação em Ciência da Computação da Universidade Estadual do Ceará (PPGCC-UECE), além de Doutor em Engenharia de Software pela University College London (UCL), Mestre em Engenharia de Software pela Universidade Estadual do Ceará (UECE) e Tecnólogo em Telemática pelo Instituto Federal do Ceará (IFCE)" }
+    ],
+
+    3: [
+        { img: "img/palestras/camila.png", palestrante: "Camila Colares", titulo: "10 coisas para NÃO fazer quando você é um estudante de computação (com exemplos reais e práticos)", local: "Auditório", horario: "", sobre: "A palestrante Camila Colares é formada em Ciências da Computação pela UECE e cursa o Doutorado em Ciências da Computação na UECE. Ocupa o cargo de Especialista Técnico em TI no Banco do Nordeste do Brasil, atuando com inteligência de dados e apoiando os processos decisórios do Banco. Já foi tech lead da Traive Finance, professora do curso de Computação da UECE, professora e coordenadora dos cursos de tecnologia da Faculdade CDL e desenvolvedora na Secretaria da Saúde do Estado do Ceará." },
+        { img: "img/palestras/lucas.png", palestrante: "Lucas Robson", titulo: "Da Ideia ao Lançamento: Aurora + Vibe Coding em um SaaS", local: "Auditório", horario: "", sobre: "Lucas Robson é um engenheiro de Software com mais de 9 anos de experiência em sistemas de comunicação e 6 anos como empreendedor, tendo fundado cinco startups. As quais três delas validadas com sucesso e gerando receita. Especialista em inovação tecnológica, gestão estratégica e validação de negócios. Atualmente CEO da FollowOP, uma startup que oferece soluções de inteligência artificial para vendas, automatizando atendimentos via WhatsApp." }
+    ],
+
+    4: [
+        { img: "img/palestras/ieee.png", palestrante: "IEEE RAS UFC", titulo: "Gêmeos Digitais na Robótica", local: "Auditório", horario: "", sobre: "A IEEE RAS UFC é um capítulo técnico estudantil da sociedade de robótica e automação, tendo como foco principal o desenvolvimento de projetos técnicos na área da robótica. O grupo é atualmente associado ao Lamotriz, laboratório de eficiência elétrica em força motriz, onde desenvolve suas pesquisas." },
+        { img: "img/palestras/eduardo.png", palestrante: "Eduardo Tavares", titulo: "RTLS: Oportunidades e rotas tecnológicas para o desenvolvimento de soluções de microlocalização para a indústria e o varejo", local: "Auditório", horario: "", sobre: "Eduardo Tavares é consultor de Estratégia e Inovação há 25 anos, com experiência em diversas áreas, tais como Corporate Venture, Venture Building, implantação de processos de gestão da inovação e criatividade e Inteligência Competitiva. Na gestão pública, atuou em projetos de saúde, construção civil, telecomunicações, geração e distribuição de energia, tecnologia da informação e varejo. Doutor em Biotecnologia com mestrado em Inovação, é fundador e co-fundador de diversas empresas, como TAQUION INOVAÇÃO, LABWORKS e INDOORSENSE. É coordenador de projetos de P&D fomentados por instituições nacionais aplicados à saúde, indústria 4.0 e varejo. Também atua como professor da Universidade Estadual do Ceará - UECE nas cadeiras de administração avançada e gestão da inovação." },
+        { img: "img/palestras/carlos.png", palestrante: "Francisco Carlos", titulo: "Diminuindo o Gap Entre Design e Dev com o Laum", local: "Auditório", horario: "", sobre: "Francisco Carlos de Mattos Brito Oliveira formou-se em Ciência da Computação pela Universidade Estadual do Ceará em 1989, e fez mestrado também em Informática Aplicada pela Universidade de Fortaleza em 2001. Em 2010, foi PhD pela Virginia Tech em 2010 em Ciência da Computação. Seu doutorado nos Estados Unidos foi viabilizado com uma bolsa Fulbright (prêmio de prestígio nos Estados Unidos) ficando em primeiro lugar no Brasil. Sua tese de doutorado foi  viabilizada pela bolsa Fulbright, foi sobre a capacitação de salas de aula convencionais inclusivas por meio da tecnologia. Francisco atua, também, como Cientista-Chefe do LDI – Laboratório de Desenvolvimento e Inovação  e como consultor ad-hoc do Banco Mundial." },
+    ]
+};
+
+const containerPalestras = document.getElementById("div-InfoPalestra");
+const containerPopUp = document.getElementById("div-popUpInfo");
+
+function mostrarPalestra(dia) {
+    containerPalestras.innerHTML = "";
+
+    const palestras = diasPalestra[dia];
+    for (let i = 0; i < palestras.length; i++) {
+        containerPalestras.innerHTML += `
+            <div class="div-CardPalestra">
+                <img src="${palestras[i].img}" alt="" class="img-InfoPalestra">
+                <h3 style="color: #037DC3;">${palestras[i].palestrante}</h3>
+                <img src="img/simbolo-de-informacao.png" alt="" class="img-bttInfoPalestra" data-index="${i}">
+                <h3 style="margin-top: 10px;">${palestras[i].titulo}</h3>
+            </div>
+        `;
+    }
+
+    const bttsInfo = document.getElementsByClassName("img-bttInfoPalestra");
+    for (let i = 0; i < bttsInfo.length; i++) {
+        bttsInfo[i].addEventListener("click", function () {
+            abrirInfos(palestras[i]);
+        });
+    }
+}
+
+const dia = 1;
+const palestrasDoDia = diasPalestra[dia];
+
+function abrirInfos(palestra) {
+    containerPopUp.style.display = "block";
+    containerPopUp.classList.add("open");
+    containerPopUp.innerHTML = `
+        <img src="img/x.png" style="height: 20px; float: right; cursor: pointer;" id="bttFechar">
+        <h2>Sobre</h2>
+        <p>${palestra.sobre}</p>
+        <h3>Local</h3>
+        <p>${palestra.local}</p>
+        <h3>Horário</h3>
+        <p>${palestra.horario}</p>
+    `;
+
+    document.getElementById("bttFechar").addEventListener("click", fecharInfo);
+}
+
+
+function fecharInfo() {
+    containerPopUp.style.display = "none";
+}
+
+const botoesDiaPalestra = document.getElementsByClassName("bttDiaPalestra");
+
+for (let i = 0; i < botoesDiaPalestra.length; i++) {
+    botoesDiaPalestra[i].addEventListener("click", function () {
+        for (let j = 0; j < botoesDiaPalestra.length; j++) {
+            botoesDiaPalestra[j].classList.remove("ativo");
+        }
+
+        this.classList.add("ativo");
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    mostrarPalestra(1);
+});
